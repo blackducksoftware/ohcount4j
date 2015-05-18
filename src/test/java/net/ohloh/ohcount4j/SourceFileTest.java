38de66ba4310c4e99462dd50b2cdf8387928bf2a
mpujari <mahesh.p@imaginea.com>
2015-05-18 21:23:39 +0530

    Adding one more simple test case

--- src/test/java/net/ohloh/ohcount4j/SourceFileTest.java
+++ src/test/java/net/ohloh/ohcount4j/SourceFileTest.java
@@ -42,6 +42,22 @@ public class SourceFileTest {
     }
 
     @Test
+    public void sourceBufferTestReadMinFirst() throws IOException {
+        String buffer = "This is a string";
+        try (SourceFile source = new SourceFile("/foo/bar.baz", buffer)) {
+            assertEquals("/foo/bar.baz", source.getPath());
+            assertEquals("bar.baz", source.getName());
+            assertEquals("baz", source.getExtension());
+            assertEquals("", source.head(0));
+            for (int i = buffer.length() + 10; i >= 0; i--) {
+                int strIndex = (i < buffer.length()) ? i : buffer.length();
+                String expectedValue = buffer.substring(0, strIndex);
+                assertEquals(expectedValue, source.head(i));
+            }
+        }
+    }
+
+    @Test
     public void sourceBufferTestWityEmptySource() throws IOException {
         try (SourceFile source = new SourceFile("/foo/bar.baz", "")) {
             assertEquals("/foo/bar.baz", source.getPath());
