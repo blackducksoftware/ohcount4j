08831ddd4c430f9a9a5d3c8893c66073b8fc5eed
Mahesh Pujari <mahesh.p@imaginea.com>
2015-12-16 14:21:31 +0530

    Changes for OHCOUNT-12 (Add ability to identify Autoconf)

--- src/main/java/net/ohloh/ohcount4j/Language.java
+++ src/main/java/net/ohloh/ohcount4j/Language.java
@@ -6,6 +6,7 @@ import java.util.List;
 import net.ohloh.ohcount4j.scan.ActionScriptScanner;
 import net.ohloh.ohcount4j.scan.AdaScanner;
 import net.ohloh.ohcount4j.scan.AssemblyScanner;
+import net.ohloh.ohcount4j.scan.AutoconfScanner;
 import net.ohloh.ohcount4j.scan.BinaryScanner;
 import net.ohloh.ohcount4j.scan.BooScanner;
 import net.ohloh.ohcount4j.scan.CStyleScanner;
@@ -51,9 +52,9 @@ public enum Language implements LanguageCategory {
 
     /*
      * All languages must be defined here.
-     * 
+     *
      * Each language must declare three mandatory properties:
-     * 
+     *
      * - The language's official display name (niceName)
      * - The category of the language, one of BUILD, LOGIC, MARKUP, UNKNOWN
      * - A Scanner subclass capable of parsing this language
@@ -63,6 +64,7 @@ public enum Language implements LanguageCategory {
     ASPX_CSHARP("ASP.NET (C#)", LOGIC, GenericCodeScanner.class), // TODO.
     ASPX_VB("ASP.NET (VB)", LOGIC, GenericCodeScanner.class), // TODO.
     ASSEMBLY("Assembly", LOGIC, AssemblyScanner.class),
+    AUTOCONF("Autoconf", BUILD, AutoconfScanner.class),
     BINARY("Binary", LOGIC, BinaryScanner.class),
     BOO("Boo", LOGIC, BooScanner.class),
     C("C", LOGIC, CStyleScanner.class),
@@ -124,10 +126,10 @@ public enum Language implements LanguageCategory {
 
     /*
      * Optional properties of languages are declared here.
-     * 
+     *
      * At a minimum, a language should define one or more file
      * extensions or filenames associated with the language.
-     * 
+     *
      * You may also declare additional names (beyond the uname
      * and niceName) by which the language might be known.
      * These aliases can be matched against things like Emacs
