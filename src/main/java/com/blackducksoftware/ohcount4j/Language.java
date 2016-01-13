7fe455a089c6e965a50e7ffc5720b8057fd34a52
Mahesh Pujari <mahesh.p@imaginea.com>
2016-01-13 21:13:22 +0530

    Adding missing file extensions for ebuild

--- src/main/java/com/blackducksoftware/ohcount4j/Language.java
+++ src/main/java/com/blackducksoftware/ohcount4j/Language.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2016 Black Duck Software, Inc.
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -79,9 +79,9 @@ public enum Language implements LanguageCategory {
 
     /*
      * All languages must be defined here.
-     *
+     * 
      * Each language must declare three mandatory properties:
-     *
+     * 
      * - The language's official display name (niceName)
      * - The category of the language, one of BUILD, LOGIC, MARKUP, UNKNOWN
      * - A Scanner subclass capable of parsing this language
@@ -170,10 +170,10 @@ public enum Language implements LanguageCategory {
 
     /*
      * Optional properties of languages are declared here.
-     *
+     * 
      * At a minimum, a language should define one or more file
      * extensions or filenames associated with the language.
-     *
+     * 
      * You may also declare additional names (beyond the uname
      * and niceName) by which the language might be known.
      * These aliases can be matched against things like Emacs
@@ -209,7 +209,7 @@ public enum Language implements LanguageCategory {
         CSS.extension("css");
         CUDA.extensions("cu", "cuh");
         D.extension("d");
-        EBUILD.extension("ebuild");
+        EBUILD.extensions("ebuild", "kdebuild-1", "eclass");
         ECMASCRIPT.extension("es");
         EIFFEL.extension("e");
         ERLANG.extension("erl");
